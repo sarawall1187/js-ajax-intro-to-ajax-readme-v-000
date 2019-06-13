2cbd1b7759fc2a7aa311a430dd4779ca320e8c82
Sara Walker <sarawall1187@gmail.com>
2019-06-13 20:56:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
