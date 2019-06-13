94e1657e18731d79903647cbb0b29749ab9290fb
Sara Walker <sarawall1187@gmail.com>
2019-06-13 21:05:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,3 +21,12 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
