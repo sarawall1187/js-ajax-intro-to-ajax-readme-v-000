b8cced59d739e2387956a84980dcfb87d79e02c8
Sara Walker <sarawall1187@gmail.com>
2019-06-13 21:06:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,6 +22,20 @@ function getRepositories() {
   req.send();
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
 
 function getCommits(el) {
   const name = el.dataset.repo;
